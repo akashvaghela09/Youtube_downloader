9ccd8562ac239327be396ed2bc966537d5576b9a
akashvaghela09 <akashvaghela09@gmail.com>
2021-05-02 09:56:43 +0530

    files added

--- src/Backend/server.js
+++ src/Backend/server.js
@@ -1,4 +1,17 @@
 const express = require("express")
+const ytdl = require("ytdl-core")
 const app = express()
 
 app.use(express.json())
+
+app.get("/", function(req, res){
+    res.sendFile(__dirname + "../Components/index.html")
+})
+
+app.get("/videoinfo", async function(req, res){
+    const videoURL = req.query.videoURL
+    const info = await ytdl.getInfo(videoURL)
+    res.status(200).json(info)
+})
+
+app.listen(5000)
